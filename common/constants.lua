0050372c78036304b9c46bbc711843b793f279fb
Glastis <glastis@glastis.com>
2023-06-11 22:19:08 +0200

    feat: added turtle inventory dimensions

--- common/constants.lua
+++ common/constants.lua
@@ -1,5 +1,7 @@
 local constants = {}
 
-constants.TURTLE_INVENTORY_SIZE = 16
+constants.TURTLE_INVENTORY_LINE = 4
+constants.TURTLE_INVENTORY_COLUMN = 4
+constants.TURTLE_INVENTORY_SIZE = constants.TURTLE_INVENTORY_COLUMN * constants.TURTLE_INVENTORY_LINE
 
 return constants
